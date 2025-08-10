0f596725166be901afa83ca976e95a88daaf86e4
shengfzy <shengfzysk12345@163.com>
2025-08-10 21:04:20 +0800

    Update thread/src/main.cpp

--- thread/src/main.cpp
+++ thread/src/main.cpp
@@ -8,6 +8,10 @@ std::mutex mtx;
 
 int cnt = 0;
 
+std::condition_variable cv;
+std::mutex mtx1;
+bool ready = false;
+
 int safeFuncWithLockGuard() {
     std::lock_guard<std::mutex> lk(mtx);
     std::cout << std::this_thread::get_id() << "cnt = " << cnt << std::endl;
@@ -17,6 +21,9 @@ int safeFuncWithLockGuard() {
 class ClassThread {
 public:
     void operator() (int args) const {
+        std::unique_lock<std::mutex> lk_s(mtx1);
+        cv.wait(lk_s, [](){return ready;});
+        lk_s.unlock();
         std::cout << "Class Thread Start" << std::endl;
         while(1) {
             (void) safeFuncWithLockGuard();
@@ -27,6 +34,9 @@ public:
 };
 
 void workthread(int args) {
+    std::unique_lock<std::mutex> lk_s(mtx1);
+    cv.wait(lk_s, []{return ready;});
+    lk_s.unlock();
     std::cout << "Work Thread Start" << std::endl;
     while(1) {
         (void) safeFuncWithLockGuard();
@@ -40,7 +50,12 @@ int main(int argc, char **argv) {
     std::thread t1(ClassThread(), 1);
     std::thread t2(workthread, 2);
     std::thread t3([](int args){
+        std::this_thread::sleep_for(std::chrono::milliseconds(5000));
         std::cout << "lambada Thread Start" << std::endl;
+        std::unique_lock<std::mutex> lk_s(mtx1);
+        ready = true;
+        cv.notify_all();
+        lk_s.unlock();
         while(1) {
             (void) safeFuncWithLockGuard();
             std::this_thread::sleep_for(std::chrono::milliseconds(1000));
